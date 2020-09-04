9050120385f4ec01b2d0a8cf32ec11ecee7b95a2
Barbara Moore <barbaraloismoore@gmail.com>
2020-09-04 17:17:07 -0400

    completed mvp ex1 ex2 ex 3

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,19 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+cache = {}
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
-    return route
+    # for each ticket, add it to the cache with key as source, value as dest
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+    # start at the ticket without a previous flight, get its dest
+    starter = cache['NONE']
+    log = [starter]
+    # while the destination isn't none 
+    while (cache[starter] != 'NONE'):
+        # add the start location to the list, wire up the dest as the new start
+        log.append(cache[starter])
+        starter = cache[starter]
+    log.append('NONE')
+    return log
